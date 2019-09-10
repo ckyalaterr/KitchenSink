8f530b8354fdee3ab26a8f35d9be648c50f087d5
ckyalaterr <54908203+ckyalaterr@users.noreply.github.com>
2019-09-10 17:00:04 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ let numberOfStates = 50;
 console.log(numberOfStates);
 
 let answer= 5+4;
-console.log(answer); //answer adds 5 + 4
+console.log(answer); //answer adds 5 and 4
 
-func
+function sayHello(){
+    console.log('Hello World!'); //function sayHello logs Hello World! when called
+}
+sayHello(); //call sayHello function
+
+function checkAge(name, age){
+    if age >= 21 console.log('Sorry ' + name + ', you are not old enough to view this page!')
+
+}
+checkAge( Ckelsie, 18);
